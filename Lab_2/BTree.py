532fa5208948c662c1c85fd3c5b6b3e79d39eb00
Dmytro Hushchin <dhuchchin@gmail.com>
2021-10-13 11:34:29 +0300

    Binary search in nodes

--- Lab_2/BTree.py
+++ Lab_2/BTree.py
@@ -26,12 +26,12 @@ class BTree:
 			new_root.children.insert(0, current_root) #Former root becomes 0th child of new root 'temp'
 			self._split_child(new_root, 0) #And we split it
 		self._insert_nonfull(self.root, key)
-    
-    
+	
+	
 	def _insert_nonfull(self, current_node : Node, key : tuple) -> None:
 		i = len(current_node.keys) - 1
 		if current_node.leaf:
-            #find index and insert (key, value)
+			#find index and insert (key, value)
 			current_node.keys.append((None, None))
 			while i >= 0 and key[0] < current_node.keys[i][0]:
 				current_node.keys[i + 1] = current_node.keys[i]
@@ -54,7 +54,7 @@ class BTree:
 		second_half = Node(first_half.leaf)
 		parent_node.children.insert(split_index + 1, second_half) #new empty node
 		parent_node.keys.insert(split_index, first_half.keys[order - 1]) #move median key to parent
-        #distribute keys between two parts
+		#distribute keys between two parts
 		second_half.keys = first_half.keys[order : 2 * order - 1]
 		first_half.keys = first_half.keys[0 : order - 1]
 		if not first_half.leaf: #distribute children between two parts
@@ -62,18 +62,22 @@ class BTree:
 			first_half.children = first_half.children[0 : order - 1]
 
 	
-	def search(self, key : tuple, current_node : Node = None):
+	def search(self, key : int, current_node : Node = None):
 		if current_node != None:
-			i = 0
-			while i < len(current_node.keys) and key > current_node.keys[i][0]:
-				i += 1
-			if i < len(current_node.keys) and key == current_node.keys[i][0]:
-				return (current_node, i)
-			elif current_node.leaf:
-				return None
-			else:
-				#Search in children
-				return self.search(key, current_node.children[i])
+			low = 0  
+			high = len(current_node.keys) - 1
+			mid = 0
+			while low <= high:
+				mid = (high + low) // 2
+				if current_node.keys[mid][0] == key:
+					return (key, current_node.keys[mid][1])
+				elif current_node.keys[mid][0] < key:  
+					low = mid + 1  
+				else:  
+					high = mid - 1
+			if current_node.leaf == False:
+				return self.search(key, current_node.children[mid + 1])
+			return None			
 		else:
-			#Search entire tree as node not provided
+			#Search entire tree if node not provided
 			return self.search(key, self.root)
--- Lab_2/main.py
+++ Lab_2/main.py
@@ -5,6 +5,7 @@ def main():
 	for i in range(20):
 		B.insert((i, str(2*i)))
 	B.print_tree(B.root)
+	print(B.search(7))
 
 if __name__ == '__main__':
 	main()
