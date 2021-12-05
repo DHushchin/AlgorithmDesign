94b3e4813e16bb23be360634c77b487e06797f6f
Dmytro Hushchin <dhuchchin@gmail.com>
2021-12-05 15:10:06 +0200

    Game Main Menu

---
+++ Lab_5/Obstruction/DEFINITIONS.hpp
@@ -0,0 +1,10 @@
+#pragma once
+
+#define SCREEN_WIDTH 768
+#define SCREEN_HEIGHT 836
+#define SPLASH_STATE_SHOW_TIME 3
+#define SPLASH_SCENE_BACKGROUND_FILEPATH "Resources\\res\\Splash Background.png"
+#define MAIN_MENU_BACKGROUND_FILEPATH "Resources\\res\\Main Menu Background.png"
+#define MAIN_MENU_TITLE_PATH "Resources\\res\\Game Title.png"
+#define MAIN_MENU_PLAY_BUTTON "Resources\\res\\Play Button.png"
+#define MAIN_MENU_PLAY_BUTTON_OUTER "Resources\\res\\Play Button Outer.png"
---
+++ Lab_5/Obstruction/Obstruction.cpp
@@ -0,0 +1,11 @@
+﻿#include <iostream>
+#include "Game.hpp"
+#include "DEFINITIONS.hpp"
+
+int main()
+{
+    Sonar::Game game(SCREEN_WIDTH, SCREEN_HEIGHT, "Obstruction Game");
+    return EXIT_SUCCESS;
+}
+
+
